98502ed0a3b854e4fafbe2407b152c4e2c61aaa0
Pamodi <pamo9055@gmail.com>
2024-02-18 17:01:34 -0500

    Add test for GET Api

--- main_test.go
+++ main_test.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetItems(t *testing.T) {
+	req, err := http.NewRequest("GET", "/items", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(itemsHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Item handler returned incorrect status code: got %v, expected %v", status, http.StatusOK)
+	}
+}
