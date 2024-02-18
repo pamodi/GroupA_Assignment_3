3fbe52874afba9517b82a4becff9b218446173d6
Pamodi <pamo9055@gmail.com>
2024-02-18 17:04:54 -0500

    Add test for POST Api

--- main_test.go
+++ main_test.go
@@ -1,11 +1,16 @@
+// Created by Balangoda Pamodi - 500229522
+// main_test file to test GET and POST Apis
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+// Function to test get items API
 func TestGetItems(t *testing.T) {
 	req, err := http.NewRequest("GET", "/items", nil)
 	if err != nil {
@@ -18,6 +23,31 @@ func TestGetItems(t *testing.T) {
 	handler.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("Item handler returned incorrect status code: got %v, expected %v", status, http.StatusOK)
+		t.Errorf("Items handler returned incorrect status code: got %v, expected %v", status, http.StatusOK)
+	}
+}
+
+// Function to test add items API
+func TestAddItem(t *testing.T) {
+	item := Item{Name: "New Item"}
+	body, _ := json.Marshal(item)
+	req, err := http.NewRequest("POST", "/items", bytes.NewBuffer(body))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(itemsHandler)
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusCreated {
+		t.Errorf("Items handler returned incorrect status code: got %d, expected %d", http.StatusCreated, rr.Code)
+	}
+
+	var newItem Item
+	json.Unmarshal(rr.Body.Bytes(), &newItem)
+	if newItem.Name != item.Name {
+		t.Errorf("Items handler returned incorrect item name: got %s, expected %s", newItem.Name, item.Name)
 	}
 }
